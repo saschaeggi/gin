9f9253094429482453e245afc2b3b8e1766bed3d
Mario Schnauss <mario.schnauss@unic.com>
2019-10-04 15:56:35 +0200

    Initial commit ... Gin!

---
+++ README.md
@@ -0,0 +1,19 @@
+# Gin Webpack 4 Build
+A minimalistic build task to compile css/js for Gin
+
+## Run build task (compile / watch / build)
+Install (run outside of vagrant):
+```
+nvm use
+npm install
+```
+
+Run dev env with watcher and debug output (development process)
+```
+npm run dev
+```
+
+Compile & minify assets
+```
+npm run prod
+```
---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const isDev = (process.env.NODE_ENV !== 'production');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const globImporter = require('node-sass-glob-importer');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: {
+    gin: ['./js/gin.js', './styles/gin.scss'],
+  },
+  output: {
+    devtoolLineToLine: true,
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: 'js/async/[name].chunk.js',
+    pathinfo: true,
+    filename: 'js/[name].js',
+    publicPath: '/themes/unic/dist/'
+  },
+  module: {
+    rules: [{
+        test: /\.(config.js)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: './'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]?[hash]',
+            },
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              enabled: !isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /modernizrrc\.js$/,
+        loader: 'expose-loader?Modernizr!webpack-modernizr-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isDev,
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+              sourceMap: isDev,
+              // minimize: true
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              importer: globImporter(),
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+    extensions: ['.js', '.json'],
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    })
+  ],
+  watchOptions: {
+    aggregateTimeout: 300,
+  }
+};
