506b3dcb8beea27e945eb74d76615a186b6237c5
Sascha Eggenberger <hello@saschaeggenberger.com>
2020-03-07 00:07:45 +0100

    Gin Future UI: update Active sidebar items,  Help

--- js/gin_toolbar.js
+++ js/gin_toolbar.js
@@ -2,8 +2,36 @@
   Drupal.behaviors.ginToolbarChange = {
     attach: function attach(context, settings) {
       // Check if on node edit form
-      if ($('body', context).hasClass('path-node')) {
+      if (window.location.href.indexOf('admin/content') > -1) {
         $('.toolbar-icon-system-admin-content').addClass('is-active');
+      }
+      // If Structure
+      else if (window.location.href.indexOf('admin/structure') > -1) {
+        $('.toolbar-icon-system-admin-structure').addClass('is-active');
+      }
+      // If Appearance
+      else if (window.location.href.indexOf('admin/appearance') > -1 || window.location.href.indexOf('admin/theme') > -1) {
+        $('.toolbar-icon-system-themes-page').addClass('is-active');
+      }
+      // If Modules
+      else if (window.location.href.indexOf('admin/modules') > -1) {
+        $('.toolbar-icon-system-modules-list').addClass('is-active');
+      }
+      // If Configuration
+      else if (window.location.href.indexOf('admin/config') > -1) {
+        $('.toolbar-icon-system-admin-config').addClass('is-active');
+      }
+      // If People
+      else if (window.location.href.indexOf('admin/people') > -1) {
+        $('.toolbar-icon-entity-user-collection').addClass('is-active');
+      }
+      // If Reports
+      else if (window.location.href.indexOf('admin/reports') > -1) {
+        $('.toolbar-icon-system-admin-reports').addClass('is-active');
+      }
+      // If Help
+      else if (window.location.href.indexOf('admin/help') > -1) {
+        $('.toolbar-icon-help-main').addClass('is-active');
       }
 
       // Change when clicked
