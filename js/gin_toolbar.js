5b2b78f9f0a82c9eee6c699da4ac3b59621da5d5
saschaeggi <saschaeggi@1999056.no-reply.drupal.org>
2020-03-20 16:39:23 +0100

    Issue #3121206 by saschaeggi: Refactor gin_toolbar.js to use Drupal Settings path

--- js/gin_toolbar.js
+++ js/gin_toolbar.js
@@ -1,36 +1,38 @@
 (function ($, Drupal) {
   Drupal.behaviors.ginToolbarActiveItem = {
     attach: function attach(context, settings) {
+      var path = settings.path.currentPath;
+
       // Check if on node edit form
-      if (window.location.href.indexOf('admin/content') > -1) {
+      if (path.indexOf('admin/content') > -1 || path.indexOf('/edit') > -1) {
         $('.toolbar-icon-system-admin-content').addClass('is-active');
       }
       // If Structure
-      else if (window.location.href.indexOf('admin/structure') > -1) {
+      else if (path.indexOf('admin/structure') > -1) {
         $('.toolbar-icon-system-admin-structure').addClass('is-active');
       }
       // If Appearance
-      else if (window.location.href.indexOf('admin/appearance') > -1 || window.location.href.indexOf('admin/theme') > -1) {
+      else if (path.indexOf('admin/appearance') > -1 || path.indexOf('admin/theme') > -1) {
         $('.toolbar-icon-system-themes-page').addClass('is-active');
       }
       // If Modules
-      else if (window.location.href.indexOf('admin/modules') > -1) {
+      else if (path.indexOf('admin/modules') > -1) {
         $('.toolbar-icon-system-modules-list').addClass('is-active');
       }
       // If Configuration
-      else if (window.location.href.indexOf('admin/config') > -1) {
+      else if (path.indexOf('admin/config') > -1) {
         $('.toolbar-icon-system-admin-config').addClass('is-active');
       }
       // If People
-      else if (window.location.href.indexOf('admin/people') > -1) {
+      else if (path.indexOf('admin/people') > -1) {
         $('.toolbar-icon-entity-user-collection').addClass('is-active');
       }
       // If Reports
-      else if (window.location.href.indexOf('admin/reports') > -1) {
+      else if (path.indexOf('admin/reports') > -1) {
         $('.toolbar-icon-system-admin-reports').addClass('is-active');
       }
       // If Help
-      else if (window.location.href.indexOf('admin/help') > -1) {
+      else if (path.indexOf('admin/help') > -1) {
         $('.toolbar-icon-help-main').addClass('is-active');
       }
     }
@@ -62,19 +64,6 @@
           $('body').attr('data-toolbar-menu', '');
         }
       });
-
-      // Toolbar hover toggle
-      // $('.toolbar-menu-administration', context).hover(function(e) {
-      //   e.preventDefault();
-
-      //   $(this).toggleClass('is-active');
-
-      //   if ($(this).hasClass('is-active')) {
-      //     $('body').attr('data-toolbar-menu', 'open');
-      //   } else {
-      //     $('body').attr('data-toolbar-menu', '');
-      //   }
-      // });
     }
   };
 
@@ -103,4 +92,4 @@
       });
     }
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
