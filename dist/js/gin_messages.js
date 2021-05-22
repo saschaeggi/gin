230f1451cbca59dede5727f92e742eb2de5bc77a
Sascha Eggenberger <hello@saschaeggenberger.com>
2021-05-22 04:06:50 +0200

    Add context to JS

--- dist/js/gin_messages.js
+++ dist/js/gin_messages.js
@@ -1,7 +1,7 @@
 !function($, Drupal, drupalSettings) {
   Drupal.behaviors.ginMessagesDismiss = {
-    attach: function(context, settings) {
-      $(".messages .button--dismiss").once("messages-dismiss").click((function(event) {
+    attach: function(context) {
+      $(".messages .button--dismiss", context).once("messages-dismiss").click((function(event) {
         event.preventDefault();
         var $elem = $(this).parents(".messages-list__item");
         $elem.css("opacity", 0), $elem.bind("transitionend", (function() {
