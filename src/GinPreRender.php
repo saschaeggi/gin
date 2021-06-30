e8ff2080daa4f1367512a73e31bc15bc07958bb5
Volker Killesreiter <killesre@gmail.com>
2021-06-30 12:11:33 +0200

    handle textarea element

---
+++ src/GinPreRender.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\gin;
+
+use Drupal\Core\Security\TrustedCallbackInterface;
+
+/**
+ * Implements trusted prerender callbacks for the Gin theme.
+ *
+ * @internal
+ */
+class GinPreRender implements TrustedCallbackInterface {
+
+  /**
+   * Prepare description toggle for output in template.
+   */
+  public static function textFormat($element) {
+    /** @var \Drupal\gin\GinSettings $settings */
+    $settings = \Drupal::classResolver(GinSettings::class);
+
+    if ($settings->get('show_description_toggle') && !empty($element['#description'])) {
+      if ($element['#type'] === 'text_format') {
+        $element['value']['#description_toggle'] = TRUE;
+      }
+      else {
+        $element['#description_toggle'] = TRUE;
+        $element['#description_display'] = 'invisible';
+
+      }
+
+    }
+     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return [
+      'textFormat',
+    ];
+  }
+
+}
