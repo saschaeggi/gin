d667e0b5b07d70bf597a6087c4aa4fa832ddcac5
saschaeggi <saschaeggi@1999056.no-reply.drupal.org>
2021-11-06 12:14:29 +0000

    Issue #3245058: Dark Mode gets removed after checking the source on CKEditor

--- js/gin_ckeditor.js
+++ js/gin_ckeditor.js
@@ -4,13 +4,14 @@
 
 (($, Drupal, drupalSettings) => {
   Drupal.behaviors.ginCKEditorContextMenu = {
-    attach: function attach() {
-      if (window.CKEDITOR && CKEDITOR !== undefined) {
+    attach: function attach(context) {
+      if (typeof CKEDITOR != 'undefined') {
         // If on CKEditor config, do nothing.
         if (drupalSettings.path.currentPath.indexOf('admin/config/content/formats/manage') > -1) {
           return;
         }
 
+        // Get configs.
         const accentCss = drupalSettings.gin.accent_css_path;
         const contentsCss = drupalSettings.gin.ckeditor_css_path;
         const accentColorPreset = drupalSettings.gin.preset_accent_color;
@@ -18,15 +19,25 @@
         // Collect Gin classes.
         let ginClasses = new Array();
 
+        // Class for Darkmode.
         if (drupalSettings.gin.darkmode) {
           ginClasses.push(drupalSettings.gin.darkmode_class);
+          CKEDITOR.config.bodyClass = drupalSettings.gin.darkmode_class;
         }
 
+        // Class for Highcontrast mode.
         if (drupalSettings.gin.highcontrastmode) {
           ginClasses.push(drupalSettings.gin.highcontrastmode_class);
+          CKEDITOR.config.bodyClass = drupalSettings.gin.highcontrastmode_class;
         }
 
-        // Contextmenu styles.
+        // Content stylesheets.
+        if (CKEDITOR.config.contentsCss === undefined) {
+          CKEDITOR.config.contentsCss.push(accentCss);
+          CKEDITOR.config.contentsCss.push(contentsCss);
+        }
+
+        // Contextmenu stylesheets.
         if (CKEDITOR.config.contextmenu_contentsCss === undefined) {
           CKEDITOR.config.contextmenu_contentsCss = new Array();
           CKEDITOR.config.contextmenu_contentsCss.push(CKEDITOR.skin.getPath('editor'));
@@ -34,18 +45,34 @@
           CKEDITOR.config.contextmenu_contentsCss.push(contentsCss);
         }
 
-        CKEDITOR.on('instanceReady', function() {
-          // Main window of CKEDITOR.
-          $('.cke_wysiwyg_frame').contents().find('body').addClass(ginClasses).attr('data-gin-accent', accentColorPreset);
-        });
+        $(CKEDITOR.instances, context).once('gin_ckeditor').each(function(index, value) {
+          CKEDITOR.on('instanceReady', function() {
+            Object.entries(value).forEach(([key, editor]) => {
+              // Initial accent color.
+              $(editor.document.$)
+                  .find('body')
+                  .attr('data-gin-accent', accentColorPreset);
 
-        // Watch for changes and inject styles.
-        const observer = new MutationObserver(function() {
-          // Contextmenu.
-          $('body > .cke_menu_panel > iframe').contents().find('body').addClass(ginClasses).attr('data-gin-accent', accentColorPreset);
-        });
+              // Change from Code to Editor.
+              editor.on('mode', function() {
+                if (this.mode == 'wysiwyg') {
+                  $(editor.document.$)
+                    .find('body')
+                    .attr('data-gin-accent', accentColorPreset);
+                }
+              });
 
-        observer.observe(document.body, { childList: true });
+              // Contextual menu.
+              editor.on('menuShow', function() {
+                $('body > .cke_menu_panel > iframe')
+                  .contents()
+                  .find('body')
+                  .addClass(ginClasses)
+                  .attr('data-gin-accent', accentColorPreset);
+              });
+            });
+          });
+        });
       }
     }
   };
