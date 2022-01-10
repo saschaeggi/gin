3e69f16b530f80859dca2cf31c93e2b35ea26370
Volker Killesreiter <killesre@gmail.com>
2022-01-10 14:45:00 +0100

    use core browserlist and build

--- dist/js/accent.js
+++ dist/js/accent.js
@@ -2,8 +2,8 @@
   Drupal.behaviors.ginSticky = {
     attach: function() {
       if (document.querySelectorAll(".region-sticky").length > 0) {
-        var observer = new IntersectionObserver((_ref => {
-          var [e] = _ref;
+        const observer = new IntersectionObserver((_ref => {
+          let [e] = _ref;
           return document.querySelector(".region-sticky").classList.toggle("region-sticky--is-sticky", e.intersectionRatio < 1);
         }), {
           threshold: [ 1 ]
@@ -13,26 +13,28 @@
     }
   }, Drupal.behaviors.ginAccent = {
     attach: function() {
-      var path = drupalSettings.path.currentPath;
+      const path = drupalSettings.path.currentPath;
       if (Drupal.behaviors.ginAccent.checkDarkmode(), Drupal.behaviors.ginAccent.setFocusColor(), 
       -1 !== path.indexOf("user/login") || -1 !== path.indexOf("user/password") || -1 !== path.indexOf("user/register") || localStorage.getItem("Drupal.gin.customAccentColor")) Drupal.behaviors.ginAccent.setAccentColor(); else if (Drupal.behaviors.ginAccent.setAccentColor(), 
       "custom" === drupalSettings.gin.preset_accent_color) {
-        var accentColorSetting = drupalSettings.gin.accent_color;
+        const accentColorSetting = drupalSettings.gin.accent_color;
         localStorage.setItem("Drupal.gin.customAccentColor", accentColorSetting);
       } else localStorage.setItem("Drupal.gin.customAccentColor", "");
     },
     setAccentColor: function() {
-      var preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null, accentColorPreset = null != preset ? preset : drupalSettings.gin.preset_accent_color;
+      let preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null;
+      const accentColorPreset = null != preset ? preset : drupalSettings.gin.preset_accent_color;
       "custom" === accentColorPreset ? ($("body").attr("data-gin-accent", preset), Drupal.behaviors.ginAccent.setCustomAccentColor("custom", color)) : $("body").attr("data-gin-accent", accentColorPreset);
     },
     setCustomAccentColor: function() {
-      var preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null, accentColorSetting = null != color ? color : drupalSettings.gin.accent_color;
+      let preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null;
+      const accentColorSetting = null != color ? color : drupalSettings.gin.accent_color;
       if ("custom" === preset) {
         $("body").attr("data-gin-accent", preset);
-        var accentColor = accentColorSetting;
+        const accentColor = accentColorSetting;
         if (accentColor) {
           Drupal.behaviors.ginAccent.clearAccentColor();
-          var strippedAccentColor = accentColor.replace("#", ""), darkAccentColor = Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 65).replace("#", ""), styles = '<style class="gin-custom-colors">            [data-gin-accent="custom"] {\n              --colorGinPrimaryRGB: '.concat(Drupal.behaviors.ginAccent.hexToRgb(accentColor), ";\n              --colorGinPrimaryHover: ").concat(Drupal.behaviors.ginAccent.shadeColor(accentColor, -10), ";\n              --colorGinPrimaryActive: ").concat(Drupal.behaviors.ginAccent.shadeColor(accentColor, -15), ";\n              --colorGinAppBackgroundRGB: ").concat(Drupal.behaviors.ginAccent.hexToRgb(Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 97)), ";\n              --colorGinTableHeader: ").concat(Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 85), ';\n            }\n            .gin--dark-mode[data-gin-accent="custom"],\n            .gin--dark-mode [data-gin-accent="custom"] {\n              --colorGinPrimaryRGB: ').concat(Drupal.behaviors.ginAccent.hexToRgb(darkAccentColor), ";\n              --colorGinPrimaryHover: ").concat(Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 55), ";\n              --colorGinPrimaryActive: ").concat(Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 50), ";\n              --colorGinTableHeader: ").concat(Drupal.behaviors.ginAccent.mixColor("2A2A2D", darkAccentColor, 88), ";\n            }\n            </style>");
+          const strippedAccentColor = accentColor.replace("#", ""), darkAccentColor = Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 65).replace("#", ""), styles = `<style class="gin-custom-colors">            [data-gin-accent="custom"] {\n              --colorGinPrimaryRGB: ${Drupal.behaviors.ginAccent.hexToRgb(accentColor)};\n              --colorGinPrimaryHover: ${Drupal.behaviors.ginAccent.shadeColor(accentColor, -10)};\n              --colorGinPrimaryActive: ${Drupal.behaviors.ginAccent.shadeColor(accentColor, -15)};\n              --colorGinAppBackgroundRGB: ${Drupal.behaviors.ginAccent.hexToRgb(Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 97))};\n              --colorGinTableHeader: ${Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 85)};\n            }\n            .gin--dark-mode[data-gin-accent="custom"],\n            .gin--dark-mode [data-gin-accent="custom"] {\n              --colorGinPrimaryRGB: ${Drupal.behaviors.ginAccent.hexToRgb(darkAccentColor)};\n              --colorGinPrimaryHover: ${Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 55)};\n              --colorGinPrimaryActive: ${Drupal.behaviors.ginAccent.mixColor("ffffff", strippedAccentColor, 50)};\n              --colorGinTableHeader: ${Drupal.behaviors.ginAccent.mixColor("2A2A2D", darkAccentColor, 88)};\n            }\n            </style>`;
           $("body").append(styles);
         }
       } else Drupal.behaviors.ginAccent.clearAccentColor();
@@ -45,10 +47,11 @@
         return r + r + g + g + b + b;
       }));
       var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? "".concat(parseInt(result[1], 16), ", ").concat(parseInt(result[2], 16), ", ").concat(parseInt(result[3], 16)) : null;
+      return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
     },
     setFocusColor: function() {
-      var preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null, focusColorPreset = null != preset ? preset : drupalSettings.gin.preset_focus_color, focusColorSetting = null != color ? color : drupalSettings.gin.focus_color;
+      let preset = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null, color = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : null;
+      const focusColorPreset = null != preset ? preset : drupalSettings.gin.preset_focus_color, focusColorSetting = null != color ? color : drupalSettings.gin.focus_color;
       Drupal.behaviors.ginAccent.clearFocusColor(), $("body").attr("data-gin-focus", focusColorPreset), 
       "custom" === focusColorSetting && $("body").css("--colorGinFocus", focusColorSetting);
     },
@@ -56,7 +59,7 @@
       $("body").css("--colorGinFocus", "");
     },
     checkDarkmode: function() {
-      var darkmodeClass = drupalSettings.gin.darkmode_class;
+      const darkmodeClass = drupalSettings.gin.darkmode_class;
       window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e => {
         e.matches && "auto" === localStorage.getItem("Drupal.gin.darkmode") && $("html").addClass(darkmodeClass);
       })), window.matchMedia("(prefers-color-scheme: light)").addEventListener("change", (e => {
@@ -75,8 +78,8 @@
       return color;
     },
     shadeColor: function(color, percent) {
-      var num = parseInt(color.replace("#", ""), 16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, B = (num >> 8 & 255) + amt, G = (255 & num) + amt;
-      return "#".concat((16777216 + 65536 * (R < 255 ? R < 1 ? 0 : R : 255) + 256 * (B < 255 ? B < 1 ? 0 : B : 255) + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1));
+      const num = parseInt(color.replace("#", ""), 16), amt = Math.round(2.55 * percent), R = (num >> 16) + amt, B = (num >> 8 & 255) + amt, G = (255 & num) + amt;
+      return `#${(16777216 + 65536 * (R < 255 ? R < 1 ? 0 : R : 255) + 256 * (B < 255 ? B < 1 ? 0 : B : 255) + (G < 255 ? G < 1 ? 0 : G : 255)).toString(16).slice(1)}`;
     }
   };
 })(jQuery, Drupal, drupalSettings);
--- dist/js/init.js
+++ dist/js/init.js
@@ -1,4 +1,4 @@
-!function() {
+(() => {
   function ginInitDarkmode() {
     1 == localStorage.getItem("Drupal.gin.darkmode") || "auto" === localStorage.getItem("Drupal.gin.darkmode") && window.matchMedia("(prefers-color-scheme: dark)").matches ? document.documentElement.classList.add("gin--dark-mode") : !0 === document.documentElement.classList.contains("gin--dark-mode") && document.documentElement.classList.remove("gin--dark-mode");
   }
@@ -9,11 +9,11 @@
     localStorage.getItem("Drupal.gin.darkmode") || (localStorage.setItem("Drupal.gin.darkmode", drupalSettings.gin.darkmode), 
     ginInitDarkmode());
   })), localStorage.getItem("Drupal.gin.toolbarExpanded")) {
-    var style = document.createElement("style"), className = "gin-toolbar-inline-styles";
+    const style = document.createElement("style"), className = "gin-toolbar-inline-styles";
     if (style.className = className, "true" === localStorage.getItem("Drupal.gin.toolbarExpanded")) {
       style.innerHTML = "\n    @media (min-width: 976px) {\n      body.gin--vertical-toolbar:not([data-toolbar-menu=open]) {\n        padding-left: 240px;\n        transition: none;\n      }\n\n      .gin--vertical-toolbar .toolbar-menu-administration {\n        min-width: var(--ginToolbarWidth, 240px);\n        transition: none;\n      }\n    }\n    ";
-      var scriptTag = document.querySelector("script");
+      const scriptTag = document.querySelector("script");
       scriptTag.parentNode.insertBefore(style, scriptTag);
     } else document.getElementsByClassName(className).length > 0 && document.getElementsByClassName(className)[0].remove();
   }
-}();
+})();
