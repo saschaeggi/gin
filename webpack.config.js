4fde0a80dacea2c47bb55e1d56eb341405e3e625
Yonit Amada <yonit.amada.dev@gmail.com>
2022-04-04 17:20:41 +0200

    Gin-3256395-rtl: Adding postcss-rtl and minor styling additions to support RTL languages.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const postcssRTLCSS = require('postcss-rtlcss');
 
 module.exports = {
   entry: {
@@ -134,6 +135,7 @@ module.exports = {
               postcssOptions: {
                 plugins: [
                   autoprefixer(),
+                  postcssRTLCSS(),
                   ['postcss-perfectionist', {
                     format: 'expanded',
                     indentSize: 2,
