5dfc167f71cb811615149295c02ed635b9fe5f85
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-05-29 18:27:12 +0200

    Minor code adjustments

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -1,9 +1,7 @@
 (($, Drupal) => {
   Drupal.behaviors.ginSidebar = {
     attach: function(context) {
-      "true" === localStorage.getItem("Drupal.gin.sidebarExpanded") ? ($("body").attr("data-meta-sidebar", "open"), 
-      $(".meta-sidebar__trigger").addClass("is-active"), $(".meta-sidebar__trigger").attr("aria-expanded", "true")) : ($("body").attr("data-meta-sidebar", "closed"), 
-      $(".meta-sidebar__trigger").removeClass("is-active"), $(".meta-sidebar__trigger").attr("aria-expanded", "false")), 
+      "true" === localStorage.getItem("Drupal.gin.sidebarExpanded") ? Drupal.behaviors.ginSidebar.showSidebar() : Drupal.behaviors.ginSidebar.collapseSidebar(), 
       $(".meta-sidebar__trigger", context).once("metaSidebarToggle").on("click", (function(e) {
         e.preventDefault(), Drupal.behaviors.ginSidebar.removeInlineStyles(), Drupal.behaviors.ginSidebar.toggleSidebar($(this));
       })), $(".meta-sidebar__close, .meta-sidebar__overlay", context).once("metaSidebarClose").on("click", (function(e) {
@@ -19,10 +17,14 @@
       Drupal.behaviors.ginSidebar.removeInlineStyles(), "true" === localStorage.getItem("Drupal.gin.sidebarExpanded") && (window.innerWidth < 1024 ? Drupal.behaviors.ginSidebar.collapseSidebar() : Drupal.behaviors.ginSidebar.showSidebar());
     },
     showSidebar: function() {
-      $(".meta-sidebar__trigger").addClass("is-active"), $("body").attr("data-meta-sidebar", "open"), 
-      $(".meta-sidebar__trigger").attr("aria-expanded", "true");
+      const showLabel = Drupal.t("Show sidebar panel");
+      $(".meta-sidebar__trigger").attr("title", showLabel), $(".meta-sidebar__trigger span").html(showLabel), 
+      $(".meta-sidebar__trigger").attr("aria-expanded", "true"), $(".meta-sidebar__trigger").addClass("is-active"), 
+      $("body").attr("data-meta-sidebar", "open");
     },
     collapseSidebar: function() {
+      const hideLabel = Drupal.t("Hide sidebar panel");
+      $(".meta-sidebar__trigger").attr("title", hideLabel), $(".meta-sidebar__trigger span").html(hideLabel), 
       $(".meta-sidebar__trigger").removeClass("is-active"), $("body").attr("data-meta-sidebar", "closed"), 
       $(".meta-sidebar__trigger").attr("aria-expanded", "false");
     },
