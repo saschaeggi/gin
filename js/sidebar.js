baf9cb0a125615e0f30f29ec8542cf1170a15f66
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-05-29 18:27:12 +0200

    Minor code adjustments

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,14 +7,10 @@
     attach: function attach(context) {
       // Set meta sidebar state.
       if (localStorage.getItem('Drupal.gin.sidebarExpanded') === 'true') {
-        $('body').attr('data-meta-sidebar', 'open');
-        $('.meta-sidebar__trigger').addClass('is-active');
-        $('.meta-sidebar__trigger').attr('aria-expanded', 'true');
+        Drupal.behaviors.ginSidebar.showSidebar();
       }
       else {
-        $('body').attr('data-meta-sidebar', 'closed');
-        $('.meta-sidebar__trigger').removeClass('is-active');
-        $('.meta-sidebar__trigger').attr('aria-expanded', 'false');
+        Drupal.behaviors.ginSidebar.collapseSidebar();
       }
 
       // Toolbar toggle
@@ -59,13 +55,23 @@
       }
     },
     showSidebar: function showSidebar() {
-      // Remove attributes.
+      const showLabel = Drupal.t('Show sidebar panel');
+      // Change labels.
+      $('.meta-sidebar__trigger').attr('title', showLabel);
+      $('.meta-sidebar__trigger span').html(showLabel);
+
+      // Attributes.
+      $('.meta-sidebar__trigger').attr('aria-expanded', 'true');
       $('.meta-sidebar__trigger').addClass('is-active');
       $('body').attr('data-meta-sidebar', 'open');
-      $('.meta-sidebar__trigger').attr('aria-expanded', 'true');
     },
     collapseSidebar: function collapseSidebar() {
-      // Remove attributes.
+      const hideLabel = Drupal.t('Hide sidebar panel');
+      // Change labels.
+      $('.meta-sidebar__trigger').attr('title', hideLabel);
+      $('.meta-sidebar__trigger span').html(hideLabel);
+
+      // Attributes.
       $('.meta-sidebar__trigger').removeClass('is-active');
       $('body').attr('data-meta-sidebar', 'closed');
       $('.meta-sidebar__trigger').attr('aria-expanded', 'false');
