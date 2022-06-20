313ae3f7e2ab46be67d70b56fcb64bbc841a80f7
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-06-20 12:47:36 +0200

    Move tabledrag to once() variant, handling legacy < 9.2

--- js/overrides/tabledrag.legacy.js
+++ js/overrides/tabledrag.legacy.js
@@ -1018,7 +1018,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 (function ($, Drupal) {
   Drupal.behaviors.ginTableDrag = {
     attach: function attach(context, settings) {
-      var createItemWrapBoundaries = function createItemWrapBoundaries(index, row) {
+      var createItemWrapBoundaries = function createItemWrapBoundaries(row) {
         var $row = $(row);
         var $firstCell = $row.find('td:first-of-type').eq(0).wrapInner(Drupal.theme('tableDragCellContentWrapper')).wrapInner($(Drupal.theme('tableDragCellItemsWrapper')).addClass('js-tabledrag-cell-content'));
         var $targetElem = $firstCell.find('.js-tabledrag-cell-content');
@@ -1026,7 +1026,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
       };
 
       Object.keys(settings.tableDrag || {}).forEach(function (base) {
-        $(context).find("#".concat(base)).find('> tr.draggable, > tbody > tr.draggable').once('claroTabledrag').each(createItemWrapBoundaries);
+        once('ginTabledrag', $(context).find("#".concat(base)).find('> tr.draggable, > tbody > tr.draggable')).forEach(createItemWrapBoundaries);
       });
     }
   };
