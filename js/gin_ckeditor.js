2ad8e43e4f5af227b6f5fb04830d4c8de766f433
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-07-06 15:41:09 +0200

    Vanilla JS rewrite of gin_ckeditor.js

--- js/gin_ckeditor.js
+++ js/gin_ckeditor.js
@@ -2,142 +2,133 @@
 
 'use strict';
 
-(($, Drupal, drupalSettings) => {
+((Drupal, drupalSettings) => {
   Drupal.behaviors.ginCKEditor = {
-    attach: function attach(context) {
-      if (window.CKEDITOR && CKEDITOR !== undefined) {
-        // If on CKEditor config, do nothing.
-        if (drupalSettings.path.currentPath.indexOf('admin/config/content/formats/manage') > -1) {
-          return;
-        }
-
-        // Get configs.
-        const variablesCss = drupalSettings.gin.variables_css_path;
-        const accentCss = drupalSettings.gin.accent_css_path;
-        const contentsCss = drupalSettings.gin.ckeditor_css_path;
-        const accentColorPreset = drupalSettings.gin.preset_accent_color;
-        const accentColor = drupalSettings.gin.accent_color;
-        const darkmodeClass = drupalSettings.gin.darkmode_class;
-
-        // Class for Darkmode.
-        if (
-          localStorage.getItem('Drupal.gin.darkmode') == 1 ||
-          localStorage.getItem('Drupal.gin.darkmode') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches
-        ) {
-          CKEDITOR.config.bodyClass = darkmodeClass;
-        }
-
-        // Content stylesheets.
-        if (CKEDITOR.config.contentsCss === undefined) {
-          CKEDITOR.config.contentsCss.push(
-            variablesCss,
-            accentCss,
-            contentsCss
-          );
-        }
-
-        // Contextmenu stylesheets.
-        if (CKEDITOR.config.contextmenu_contentsCss === undefined) {
-          CKEDITOR.config.contextmenu_contentsCss = new Array();
-
-          // Check if skinName is set.
-          if (typeof CKEDITOR.skinName === 'undefined') {
-            CKEDITOR.skinName = CKEDITOR.skin.name;
+    attach: (context) => {
+      const ginCKEditor = once('ginCKEditor', context.querySelectorAll('body'));
+      ginCKEditor.forEach(() => {
+        if (window.CKEDITOR && CKEDITOR !== undefined) {
+          // If on CKEditor config, do nothing.
+          if (drupalSettings.path.currentPath.indexOf('admin/config/content/formats/manage') > -1) {
+            return;
           }
 
-          CKEDITOR.config.contextmenu_contentsCss.push(
-            CKEDITOR.skin.getPath('editor'),
-            variablesCss,
-            accentCss,
-            contentsCss
-          );
-        }
+          // Get configs.
+          const variablesCss = drupalSettings.gin.variables_css_path;
+          const accentCss = drupalSettings.gin.accent_css_path;
+          const contentsCss = drupalSettings.gin.ckeditor_css_path;
+          const accentColorPreset = drupalSettings.gin.preset_accent_color;
+          const accentColor = drupalSettings.gin.accent_color;
+          const darkmodeClass = drupalSettings.gin.darkmode_class;
 
-        $(CKEDITOR.instances, context).each(function(index, value) {
-          const $ckeditorInstance = CKEDITOR.on('instanceReady', function() {
-            Object.entries(value).forEach(([key, editor]) => {
-              // Initial accent color.
-              $(editor.document.$)
-                .find('body')
-                .attr('data-gin-accent', accentColorPreset);
+          // Class for Darkmode.
+          if (
+            localStorage.getItem('Drupal.gin.darkmode') == 1 ||
+            localStorage.getItem('Drupal.gin.darkmode') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches
+          ) {
+            CKEDITOR.config.bodyClass = darkmodeClass;
+          }
 
-              if (accentColorPreset === 'custom' && accentColor) {
-                Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, $(editor.document.$).find('head'));
-              }
+          // Content stylesheets.
+          if (CKEDITOR.config.contentsCss === undefined) {
+            CKEDITOR.config.contentsCss.push(
+              variablesCss,
+              accentCss,
+              contentsCss
+            );
+          }
 
-              // Change from Code to Editor.
-              editor.on('mode', function() {
-                if (this.mode == 'wysiwyg') {
-                  $(editor.document.$)
-                    .find('body')
-                    .attr('data-gin-accent', accentColorPreset);
+          // Contextmenu stylesheets.
+          if (CKEDITOR.config.contextmenu_contentsCss === undefined) {
+            CKEDITOR.config.contextmenu_contentsCss = new Array();
 
-                  if (accentColorPreset === 'custom' && accentColor) {
-                    Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, $(editor.document.$).find('head'));
-                  }
+            // Check if skinName is set.
+            if (typeof CKEDITOR.skinName === 'undefined') {
+              CKEDITOR.skinName = CKEDITOR.skin.name;
+            }
 
-                  if (localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
-                    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                      $(editor.document.$)
-                        .find('body')
-                        .addClass(darkmodeClass);
-                    } else {
-                      $(editor.document.$)
-                        .find('body')
-                        .removeClass(darkmodeClass);
-                    }
-                  }
-                }
-              });
+            CKEDITOR.config.contextmenu_contentsCss.push(
+              CKEDITOR.skin.getPath('editor'),
+              variablesCss,
+              accentCss,
+              contentsCss
+            );
+          }
 
-              // Contextual menu.
-              editor.on('menuShow', function() {
-                const darkModeClass = localStorage.getItem('Drupal.gin.darkmode') == 1 || localStorage.getItem('Drupal.gin.darkmode') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches
-                  ? darkmodeClass
-                  : '';
-                $('body > .cke_menu_panel > iframe')
-                  .contents()
-                  .find('body')
-                  .addClass(darkModeClass)
-                  .attr('data-gin-accent', accentColorPreset);
+          CKEDITOR.on('instanceReady', (element) => {
+            const editor = element.editor;
+            const editorBody = editor.document.$.body;
+
+            // Initial accent color.
+            editorBody.setAttribute('data-gin-accent', accentColorPreset);
+
+            if (accentColorPreset === 'custom' && accentColor) {
+              Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, document.querySelector(editor.document.$).querySelectorAll('head'));
+            }
+
+            // Change from Code to Editor.
+            editor.on('mode', function() {
+              if (this.mode == 'wysiwyg') {
+                editorBody.setAttribute('data-gin-accent', accentColorPreset);
 
                 if (accentColorPreset === 'custom' && accentColor) {
-                  Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, $('body > .cke_menu_panel > iframe').contents().find('head'));
+                  Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, document.querySelector(editor.document.$).querySelectorAll('head'));
                 }
-              });
 
-              // Toggle Darkmode.
-              window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-                if (e.matches && localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
-                  $(editor.document.$)
-                    .find('body')
-                    .addClass(darkmodeClass);
-
-                  $('body > .cke_menu_panel > iframe')
-                    .contents()
-                    .find('body')
-                    .addClass(darkmodeClass);
+                if (localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
+                  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    editorBody.classList.add(darkmodeClass);
+                  } else {
+                    editorBody.classList.remove(darkmodeClass);
+                  }
                 }
-              });
+              }
+            });
 
-              // Change to Lightmode.
-              window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', (e) => {
-                if (e.matches && localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
-                  $(editor.document.$)
-                    .find('body')
-                    .removeClass(darkmodeClass);
+            // Contextual menu.
+            editor.on('menuShow', function(element) {
+              const darkModeClass = localStorage.getItem('Drupal.gin.darkmode') == 1 || localStorage.getItem('Drupal.gin.darkmode') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? darkmodeClass
+                : '';
+              const iframeElement = element.data[0].element.$.childNodes[0].contentWindow.document;
 
-                  $('body > .cke_menu_panel > iframe')
-                    .contents()
-                    .find('body')
-                    .removeClass(darkmodeClass);
+              if (darkModeClass) {
+                iframeElement.body.classList.add(darkModeClass);
+              }
+
+              iframeElement.body.setAttribute('data-gin-accent', accentColorPreset);
+
+              if (accentColorPreset === 'custom' && accentColor) {
+                Drupal.behaviors.ginAccent.setCustomAccentColor(accentColor, elementBody.head);
+              }
+            });
+
+            // Toggle Darkmode.
+            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+              if (e.matches && localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
+                editorBody.classList.add(darkmodeClass);
+
+                if (document.querySelectorAll(`.${editor.id}.cke_panel`).length > 0) {
+                  const iframeElement = document.querySelector(`.${editor.id}.cke_panel`).childNodes[0].contentWindow.document;
+                  iframeElement.body.classList.add(darkmodeClass);
                 }
-              });
+              }
+            });
+
+            // Change to Lightmode.
+            window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
+              if (e.matches && localStorage.getItem('Drupal.gin.darkmode') === 'auto') {
+                editorBody.classList.remove(darkmodeClass);
+
+                if (document.querySelectorAll(`.${editor.id}.cke_panel`).length > 0) {
+                  const iframeElement = document.querySelector(`.${editor.id}.cke_panel`).childNodes[0].contentWindow.document;
+                  iframeElement.body.classList.remove(darkmodeClass);
+                }
+              }
             });
           });
-          once('ginCKEditor', $ckeditorInstance);
-        });
-      }
+        }
+      });
     }
   };
-})(jQuery, Drupal, drupalSettings);
+})(Drupal, drupalSettings);
