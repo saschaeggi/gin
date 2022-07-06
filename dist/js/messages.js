6e0eff2eaff99665373eccdcbb9a8b31ad1a6f6f
Sascha Eggenberger <hello@saschaeggenberger.com>
2022-07-06 15:48:09 +0200

    Vanilla JS refactor of messages.js

--- dist/js/messages.js
+++ dist/js/messages.js
@@ -1,14 +1,11 @@
-(($, Drupal, drupalSettings) => {
+(Drupal => {
   Drupal.behaviors.ginMessagesDismiss = {
-    attach: function(context) {
-      const $messagesClick = $(".messages .button--dismiss", context).click((function(event) {
-        event.preventDefault();
-        const $elem = $(this).parents(".messages-list__item");
-        $elem.css("opacity", 0), $elem.bind("transitionend", (function() {
-          $(this).addClass("visually-hidden"), $(this).css("opacity", 1);
-        }));
-      }));
-      once("messages-dismiss", $messagesClick);
+    attach: context => {
+      once("ginMessagesDismiss", context.querySelectorAll(".messages .button--dismiss")).forEach((el => el.addEventListener("click", (e => {
+        e.preventDefault();
+        const listItem = e.currentTarget.closest(".messages-list__item");
+        listItem.style.opacity = 0, listItem.classList.add("visually-hidden");
+      }))));
     }
   };
-})(jQuery, Drupal, drupalSettings);
+})(Drupal);
--- js/messages.js
+++ js/messages.js
@@ -7,20 +7,16 @@
 
 'use strict';
 
-(($, Drupal, drupalSettings) => {
+((Drupal) => {
   Drupal.behaviors.ginMessagesDismiss = {
-    attach: function(context) {
-      const $messagesClick = $('.messages .button--dismiss', context).click(function(event) {
-        event.preventDefault();
-        const $elem = $(this).parents('.messages-list__item');
-
-        $elem.css('opacity', 0);
-        $elem.bind('transitionend', function() {
-          $(this).addClass('visually-hidden');
-          $(this).css('opacity', 1);
-        })
-      });
-      once('messages-dismiss', $messagesClick);
+    attach: (context) => {
+      const ginMessagesDismiss = once('ginMessagesDismiss', context.querySelectorAll('.messages .button--dismiss'));
+      ginMessagesDismiss.forEach(el => el.addEventListener('click', e => {
+        e.preventDefault();
+        const listItem = e.currentTarget.closest('.messages-list__item');
+        listItem.style.opacity = 0;
+        listItem.classList.add('visually-hidden');
+      }));
     }
   }
-})(jQuery, Drupal, drupalSettings);
+})(Drupal);
