80baa5d4e10098bf7ea0e7083e340e20ece475c0
Florent Torregrosa <14238-florenttorregrosa@users.noreply.drupalcode.org>
2023-09-15 14:31:08 +0200

    Issue #3387572: Allow to enable description toggle in custom forms

--- src/GinDescriptionToggle.php
+++ src/GinDescriptionToggle.php
@@ -57,7 +57,7 @@ class GinDescriptionToggle implements ContainerInjectionInterface {
    *   The variables array (modify in place).
    */
   public function preprocess(array &$variables) {
-    if ($this->isEnabled()) {
+    if ($this->isEnabled() || (isset($variables['element']['#description_toggle']) && $variables['element']['#description_toggle'])) {
       if (!empty($variables['description'])) {
         $variables['description_display'] = 'invisible';
         $variables['description_toggle'] = TRUE;
