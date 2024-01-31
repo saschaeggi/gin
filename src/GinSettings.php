659428fc1a50df4b2abcd9d5767563b4622be959
Sascha Eggenberger <hello@saschaeggenberger.com>
2024-01-31 13:42:32 +0100

    Fix phpcs warning

--- src/GinSettings.php
+++ src/GinSettings.php
@@ -7,7 +7,6 @@ use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\user\UserDataInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
