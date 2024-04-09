c43749b7d0fa09f8b6a899eafb20d2113c42acf1
Sascha Eggenberger <hello@saschaeggenberger.com>
2024-04-09 19:38:25 +0200

    New approach
    Use transform to place it within overlfow, watch table header
    using an observer to place the header sticky.

--- dist/js/tableheader.js
+++ dist/js/tableheader.js
@@ -5,15 +5,37 @@
     }
   }, Drupal.ginTableHeader = {
     init: function(context) {
-      once("ginTableHeader", ".sticky-enabled", context).forEach((el => {
-        new IntersectionObserver((_ref => {
+      once("ginTableHeaderSticky", "table.position-sticky", context).forEach((el => {
+        this.prepareTableHeader(el), new IntersectionObserver((_ref => {
           let [e] = _ref;
-          context.querySelector(".gin-table-scroll-wrapper") && (e.isIntersecting || e.intersectionRect.top !== Drupal.displace.offsets.top ? context.querySelector(".gin-table-scroll-wrapper").classList.remove("--is-sticky") : context.querySelector(".gin-table-scroll-wrapper").classList.add("--is-sticky"), 
-          Drupal.displace(!0));
+          e.isIntersecting ? (window.removeEventListener("scroll", this.setScrollBehavior), 
+          this.resetScrollBehavior(el)) : window.addEventListener("scroll", (() => {
+            this.setScrollBehavior(el);
+          })), Drupal.displace(!0);
         }), {
           threshold: 1,
-          rootMargin: `-${Drupal.displace.offsets.top}px 0px 0px 0px`
+          rootMargin: "-60px 0px 0px 0px"
         }).observe(el.querySelector("thead"));
+      }));
+    },
+    prepareTableHeader: function(el) {
+      const thHeight = `${el.querySelector("thead th").offsetHeight}px`;
+      el.querySelectorAll("thead .sticky-header__content").forEach((th => {
+        th.parentNode.style.height = thHeight, th.style.height = thHeight;
+      })), window.addEventListener("DOMContentLoaded", (() => {
+        Drupal.displace(!0), this.setScrollBehavior(el);
+      }));
+    },
+    setScrollBehavior: function(el) {
+      el.querySelectorAll("thead .sticky-header__content").forEach((th => {
+        let value = 0;
+        -1 * th.parentNode.getBoundingClientRect().top >= -60 && (value = -1 * th.parentNode.getBoundingClientRect().top + Drupal.displace.offsets.top - 3), 
+        th.style.transform = `translate3d(0, ${value}px, 0)`;
+      }));
+    },
+    resetScrollBehavior: function(el) {
+      el.querySelectorAll("thead .sticky-header__content").forEach((th => {
+        th.style.transform = "";
       }));
     }
   };
--- js/tableheader.js
+++ js/tableheader.js
@@ -7,23 +7,62 @@
 
   Drupal.ginTableHeader = {
     init: function (context) {
-      once('ginTableHeader', '.sticky-enabled', context).forEach(el => {
+      // Tables with new position-sticky enabled.
+      // Fix for overflow issue.
+      once('ginTableHeaderSticky', 'table.position-sticky', context).forEach(el => {
+        // Prepare table.
+        this.prepareTableHeader(el);
+
         // Watch sticky table header.
         const observer = new IntersectionObserver(
           ([e]) => {
-            if (context.querySelector('.gin-table-scroll-wrapper')) {
-              if (!e.isIntersecting && e.intersectionRect.top === Drupal.displace.offsets.top) {
-                context.querySelector('.gin-table-scroll-wrapper').classList.add('--is-sticky');
-              } else {
-                context.querySelector('.gin-table-scroll-wrapper').classList.remove('--is-sticky');
-              }
+            if (!e.isIntersecting) {
+              window.addEventListener('scroll', () => {
+                this.setScrollBehavior(el)
+              });
 
-              Drupal.displace(true);
+            } else {
+              window.removeEventListener('scroll', this.setScrollBehavior);
+              this.resetScrollBehavior(el);
             }
+
+            Drupal.displace(true);
           },
-          { threshold: 1.0, rootMargin: `-${Drupal.displace.offsets.top}px 0px 0px 0px` }
+          { threshold: 1.0, rootMargin: `-60px 0px 0px 0px` }
         );
+
+        // Watch thead.
         observer.observe(el.querySelector('thead'));
+      });
+    },
+    prepareTableHeader: function (el) {
+      const thHeight = `${el.querySelector('thead th').offsetHeight}px`;
+
+      el.querySelectorAll('thead .sticky-header__content').forEach(th => {
+        th.parentNode.style.height = thHeight;
+        th.style.height = thHeight;
+      });
+
+      // Set inital placement after DOM is loaded.
+      window.addEventListener('DOMContentLoaded', () => {
+        Drupal.displace(true);
+        this.setScrollBehavior(el);
+      });
+    },
+    setScrollBehavior: function (el) {
+      el.querySelectorAll('thead .sticky-header__content').forEach(th => {
+        let value = 0;
+
+        if (th.parentNode.getBoundingClientRect().top * -1 >= -60) {
+          value = (th.parentNode.getBoundingClientRect().top * -1) + Drupal.displace.offsets.top - 3;
+        }
+
+        th.style.transform = `translate3d(0, ${value}px, 0)`;
+      });
+    },
+    resetScrollBehavior: function (el) {
+      el.querySelectorAll('thead .sticky-header__content').forEach(th => {
+        th.style.transform = '';
       });
     },
 
