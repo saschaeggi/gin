95ba32470e24999e7c335cb7de2b6817a30f68de
Sascha Eggenberger <hello@saschaeggenberger.com>
2024-05-15 13:55:10 +0200

    Move form actions to sticky bar

--- dist/js/edit_form.js
+++ dist/js/edit_form.js
@@ -1,7 +1,7 @@
 (Drupal => {
   Drupal.behaviors.ginEditForm = {
     attach: context => {
-      once("ginEditForm", ".region-content form.gin-node-edit-form", context).forEach((form => {
+      once("ginEditForm", ".region-content form.gin-sticky-form-actions", context).forEach((form => {
         const sticky = context.querySelector(".gin-sticky"), newParent = context.querySelector(".region-sticky__items__inner");
         if (newParent && 0 === newParent.querySelectorAll(".gin-sticky").length) {
           newParent.appendChild(sticky);
@@ -10,7 +10,9 @@
             el.setAttribute("form", form.getAttribute("id")), el.setAttribute("id", el.getAttribute("id") + "--gin-edit-form");
           })), formLabels.forEach((el => {
             el.setAttribute("for", el.getAttribute("for") + "--gin-edit-form");
-          })));
+          }))), document.querySelector("#block-gin-local-actions").querySelectorAll(".button--primary").forEach((button => {
+            button.classList.remove("button--primary"), button.classList.remove("button--secondary");
+          }));
         }
       }));
     }
