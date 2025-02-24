6fca1e29e6252fb9e6112156cde2e55e3b9a0a91
Sascha Eggenberger <hello@saschaeggenberger.com>
2025-02-24 01:15:37 +0100

    Always enable sticky_action_buttons when navigation_top_bar is active

--- src/GinContentFormHelper.php
+++ src/GinContentFormHelper.php
@@ -296,6 +296,11 @@ class GinContentFormHelper implements ContainerInjectionInterface {
     // Sets default to TRUE if setting is enabled.
     $sticky_action_buttons = $settings->get('sticky_action_buttons') ? TRUE : FALSE;
 
+    // TEST: Always enable if navigation_top_bar is active.
+    if (_gin_module_is_active('navigation_top_bar')) {
+      $sticky_action_buttons = TRUE;
+    }
+
     // API check.
     $form_ids = $this->moduleHandler->invokeAll('gin_ignore_sticky_form_actions');
     $this->moduleHandler->alter('gin_ignore_sticky_form_actions', $form_ids);
