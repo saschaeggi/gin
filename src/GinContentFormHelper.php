0acf2ecfed0ab02f8d698459768eaede33770312
Gaurav Manerkar <15032-gaurav_manerkar@users.noreply.drupalcode.org>
2025-06-17 21:44:50 +0000

    Issue #3515435: Added block form route to the list

--- src/GinContentFormHelper.php
+++ src/GinContentFormHelper.php
@@ -340,6 +340,7 @@ class GinContentFormHelper implements ContainerInjectionInterface {
     $route_names = [
       'node.add',
       'block_content.add_page',
+      'block_content.add_form',
       'entity.block_content.canonical',
       'entity.media.add_form',
       'entity.media.canonical',
