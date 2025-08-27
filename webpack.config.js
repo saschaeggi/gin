1e20427caff402aede099b84c455305b0ecda825
Jürgen Haas <21467-jurgenhaas@users.noreply.drupalcode.org>
2025-08-27 09:10:04 +0000

    Remove support for upgrade_status

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,6 @@ module.exports = {
     'components/toolbar': ['./styles/components/toolbar.scss'],
     'components/toolbar_secondary': ['./styles/components/toolbar_secondary.scss'],
     'components/tooltip': ['./styles/components/tooltip.scss'],
-    'components/upgrade_status': ['./styles/components/upgrade_status.scss'],
     // Layout
     'layout/core_navigation': ['./styles/layout/core_navigation.scss'],
     'layout/navigation': ['./styles/layout/navigation.scss'],
