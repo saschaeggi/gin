bd469efb3d66906dfe244d23e8ee6843b60627c9
Jürgen Haas <21467-jurgenhaas@users.noreply.drupalcode.org>
2025-08-27 10:13:06 +0000

    Remove support for layout_paragraphs

--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,6 @@ module.exports = {
     'components/entity_browser': ['./styles/components/entity_browser.scss'],
     'components/entity_reference_layout': ['./styles/components/entity_reference_layout.scss'],
     'components/inline_entity_form': ['./styles/components/inline_entity_form.scss'],
-    'components/layout_paragraphs': ['./styles/components/layout_paragraphs.scss'],
-    'components/layout_paragraphs_2.0': ['./styles/components/layout_paragraphs_2.0.scss'],
     'components/maintenance_page': ['./styles/components/maintenance_page.scss'],
     'components/media_library': ['./styles/components/media_library.scss'],
     'components/node_preview': ['./styles/components/node_preview.scss'],
